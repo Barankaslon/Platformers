d2f4e624ebf9ca182f46e80cb38e971822caa495
Alex <barankaslon@hotmail.com>
2021-12-30 03:17:06 -0400

    set Player Movements

--- 2D_Shooter/Assets/Scripts/PlayerController.cs
+++ 2D_Shooter/Assets/Scripts/PlayerController.cs
@@ -4,7 +4,30 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    private void Start() {
+    [SerializeField] public Rigidbody2D theRB;
+    [SerializeField] public float moveSpeed;
+    [SerializeField] public float jumpForce;
+    [SerializeField] Transform groundPoint;
+
+    public LayerMask whatIsGround;
+
+    private bool _isOnGround;
+
+
+    private void Start() 
+    {
         
+    }
+
+    private void Update() 
+    {
+        theRB.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * moveSpeed, theRB.velocity.y);
+
+        _isOnGround = Physics2D.OverlapCircle(groundPoint.position, .2f, whatIsGround);
+
+        if(Input.GetButtonDown("Jump") && _isOnGround)
+        {
+            theRB.velocity = new Vector2(theRB.velocity.x, jumpForce);
+        }
     }
 }
